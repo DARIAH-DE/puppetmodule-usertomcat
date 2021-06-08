be140516b8a6800746b0af26ec4817dcfc93a3d4
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2021-06-08 21:18:31 +0200

    use https://repo1.maven.org to get jolokia.war

--- manifests/jolokia.pp
+++ manifests/jolokia.pp
@@ -1,7 +1,7 @@
 class usertomcat::jolokia {
 
     staging::file { 'jolokia.war':
-      source  => "http://central.maven.org/maven2/org/jolokia/jolokia-war/1.3.7/jolokia-war-1.3.7.war",
+      source  => "https://repo1.maven.org/maven2/org/jolokia/jolokia-war/1.3.7/jolokia-war-1.3.7.war",
       target  => "/var/cache/jolokia.war",
     }
 
